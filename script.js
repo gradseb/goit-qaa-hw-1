a2b0154151e6fe13813cb1bc1ae2f060f8248782
gradseb <kros00133@gmail.com>
2024-03-10 20:05:51 +0100

    Drugi commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// funkcja do zmiany naglowka
+function myFunction() {
+    const element = document.getElementById("id01");
+    element.innerHTML = "Nowy nagłówek";
+}
+
+// funkcja do zmiany koloru         
+function myColorChange() {
+    const element = document.getElementById("id01");
+    element.style.color = "green";
+}
+
+//przykladowy komentarz jednolinijkowy
+
+/*
+przykladowy
+komentarz 
+wielolinijkowy
+*/
+
+// console.log('seba');
