0cb5e93c1151eaaf22678419633825ca9a12302d
gradseb <kros00133@gmail.com>
2024-03-25 21:21:56 +0100

    Zadanie z tablic

---
+++ array/array.js
@@ -0,0 +1,17 @@
+// Zadanie 1
+
+const fruits = ["Jabłko", "Banan", "Pomarańcza", "Winogrona", "Arbuz"];
+
+
+console.log(fruits);
+
+// Zadanie 2
+
+const numbers = [1, 3, 4, 7, 20, 18, 11, 2, 44, 35, 51, 5];
+
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 5) {
+    console.log(numbers[i]);
+  }
+}
