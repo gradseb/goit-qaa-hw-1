379161371fc04871e42053bf4310e244edb5fe89
gradseb <kros00133@gmail.com>
2024-04-03 22:55:46 +0200

    funkcje i metody

---
+++ Funkcje/podwojonaliczba.js
@@ -0,0 +1,9 @@
+const tab = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const tabs = tab.map(Number => Number*2)
+
+
+    console.log(tab); 
+    console.log(tabs); 
+
+
---
+++ Funkcje/students.js
@@ -0,0 +1,12 @@
+const students = [
+{firstname: 'Emily', lastname: 'Johnson'},
+{firstname: 'Oliver', lastname: 'Smith'},
+{firstname: 'Sophia', lastname: 'Williams,'},
+{firstname: 'Benjamin', lastname: 'Brown,'},
+{firstname: 'Ava', lastname: 'Davis,'},
+];
+
+const student = students.map(students => students.firstname);
+
+console.log(students);
+console.log(student);
